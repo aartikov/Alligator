7041a86a3c3db19363dc58fe8dbe041912975e7d
Artur Artikov <a.artikov@gmail.com>
2019-11-07 13:13:25 +0700

    Edit documentation

--- alligator/src/main/java/me/aartikov/alligator/FlowScreen.java
+++ alligator/src/main/java/me/aartikov/alligator/FlowScreen.java
@@ -2,7 +2,8 @@ package me.aartikov.alligator;
 
 
 /**
- * Flow screen
+ * Marks that a screen is flow screen. Should be used for fragments only. For fragments that are registered as flow screens
+ * navigation is configured by {@code flowFragmentNavigation} method of {@link me.aartikov.alligator.NavigationContext.Builder}.
  */
 public interface FlowScreen extends Screen {
 }

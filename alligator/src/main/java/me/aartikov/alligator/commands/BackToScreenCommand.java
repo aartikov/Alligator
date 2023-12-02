51a5b7a6b9df16fa9589fa8668e47a65fecf1899
Jamal <aliev.djamal.2000@gmail.com>
2023-12-02 21:54:25 +0300

    fix code style

--- alligator/src/main/java/me/aartikov/alligator/commands/BackToScreenCommand.java
+++ alligator/src/main/java/me/aartikov/alligator/commands/BackToScreenCommand.java
@@ -18,32 +18,32 @@ import me.aartikov.alligator.navigators.FragmentNavigator;
 /**
  * Command implementation for {@code goBackTo} method of {@link me.aartikov.alligator.AndroidNavigator}.
  */
-public class BackToScreenCommand extends BaseCommand{
-    private Screen mScreen;
-    @Nullable
-    private ScreenResult mScreenResult;
-    @Nullable
-    private AnimationData mAnimationData;
+public class BackToScreenCommand extends BaseCommand {
+	private Screen mScreen;
+	@Nullable
+	private ScreenResult mScreenResult;
+	@Nullable
+	private AnimationData mAnimationData;
 
-    public BackToScreenCommand(@NonNull Screen screen, @Nullable ScreenResult screenResult, @Nullable AnimationData animationData) {
-        super(screen.getClass());
-        mScreen = screen;
-        mScreenResult = screenResult;
-        mAnimationData = animationData;
-    }
+	public BackToScreenCommand(@NonNull Screen screen, @Nullable ScreenResult screenResult, @Nullable AnimationData animationData) {
+		super(screen.getClass());
+		mScreen = screen;
+		mScreenResult = screenResult;
+		mAnimationData = animationData;
+	}
 
-    @Override
-    protected void executeForActivity(@NonNull ActivityDestination destination, @NonNull ActivityNavigator activityNavigator) throws NavigationException {
-        throw new NotSupportedOperationException("BackToScreen command is not supported for activity.");
-    }
+	@Override
+	protected void executeForActivity(@NonNull ActivityDestination destination, @NonNull ActivityNavigator activityNavigator) throws NavigationException {
+		throw new NotSupportedOperationException("BackToScreen command is not supported for activity.");
+	}
 
-    @Override
-    protected void executeForFragment(@NonNull FragmentDestination destination, @NonNull FragmentNavigator fragmentNavigator) throws NavigationException {
-        fragmentNavigator.goBackTo(mScreen, destination, mScreenResult, mAnimationData);
-    }
+	@Override
+	protected void executeForFragment(@NonNull FragmentDestination destination, @NonNull FragmentNavigator fragmentNavigator) throws NavigationException {
+		fragmentNavigator.goBackTo(mScreen, destination, mScreenResult, mAnimationData);
+	}
 
-    @Override
-    protected void executeForDialogFragment(@NonNull DialogFragmentDestination destination, @NonNull DialogFragmentNavigator dialogFragmentNavigator) throws NavigationException {
-        throw new NotSupportedOperationException("BackToScreen command is not supported for dialog fragments.");
-    }
+	@Override
+	protected void executeForDialogFragment(@NonNull DialogFragmentDestination destination, @NonNull DialogFragmentNavigator dialogFragmentNavigator) throws NavigationException {
+		throw new NotSupportedOperationException("BackToScreen command is not supported for dialog fragments.");
+	}
 }

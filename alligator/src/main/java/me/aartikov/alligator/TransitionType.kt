56b0e0778c745195854d79f8c7c14f2b04adfa61
Jamal <aliev.djamal.2000@gmail.com>
2023-12-05 15:13:27 +0300

    implement kotlin in all enum

--- alligator/src/main/java/me/aartikov/alligator/TransitionType.kt
+++ alligator/src/main/java/me/aartikov/alligator/TransitionType.kt
@@ -1,12 +1,8 @@
-package me.aartikov.alligator;
-
+package me.aartikov.alligator
 
 /**
  * Type of screen transition
  */
-public enum TransitionType {
-	FORWARD,
-	BACK,
-	REPLACE,
-	RESET
+enum class TransitionType {
+    FORWARD, BACK, REPLACE, RESET
 }

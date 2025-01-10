e158f65a406a2cb8d54dd20894c607fdc178aeb0
Alidibir Akhbulatov <alidibir.akhbulatov@gmail.com>
2025-01-10 23:11:52 +0300

    Migrate build.gradle files of "alligator" and "alligator-compiler" modules

---
+++ alligator-compiler/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
+group = "com.github.aartikov"
+version = "4.4.0"
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("com.squareup:javapoet:1.10.0")
+}
+
+java {
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(11)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+        }
+    }
+}
